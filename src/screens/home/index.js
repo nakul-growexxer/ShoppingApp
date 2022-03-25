54fbb453d9fc8512fedfe98a72dbd619bb7d1af4
nakul-growexxer <nakul.kothari@growexx.com>
2022-03-25 10:47:05 +0530

    api intigration

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import {View, FlatList} from 'react-native';
 import ProductItem from '../../components/ProductItem';
 import products from '../../data/products';
 
 const HomeScreen = () => {
+  const [Product, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products/').then(({data}) => {
+      console.log(data);
+      setProducts(data);
+    });
+  }, []);
   return (
     <View>
       <FlatList
